a58c3b8b245561da4dc081e9b90b196be8b5ea43
Aidan Davies <poppers@davies.net.au>
2023-07-09 14:11:48 +1000

    Created BattleEntity

---
+++ GTMK2023 Deckbuilder/Assets/Scripts/BattleEntity.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BattleEntity : MonoBehaviour
+{
+    [SerializeField] private int _health;
+    [SerializeField] private int _healthMax;
+
+    [SerializeField] private UnityEvent _onDeath;
+
+    // PROPERTIES
+    public int Health
+	{
+        get { return _health; }
+	}
+    public int HealthMax
+	{
+        get { return _healthMax; }
+	}
+
+    // METHODS
+    public void TakeDamage(int damage)
+	{
+        _health = Mathf.Max(_health - damage, 0);
+
+        if (_health == 0) // die
+		{
+            _onDeath.Invoke(); // call all methods associated with this entity dying
+            Destroy(gameObject);
+		}
+	}
+
+    public void Heal(int healing)
+    {
+        _health = Mathf.Min(_health + healing, _healthMax);
+    }
+}

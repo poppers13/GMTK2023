cfe0261955b7fbb435fdcaa6fc1c178a6f3052c2
Aidan Davies <poppers@davies.net.au>
2023-07-09 14:53:23 +1000

    EnemyAttackStrategy can now deal damage to hero

--- GTMK2023 Deckbuilder/Assets/Scripts/Enemies/EnemyAttackStrategy.cs
+++ GTMK2023 Deckbuilder/Assets/Scripts/Enemies/EnemyAttackStrategy.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class EnemyAttackStrategy : EnemyStrategy
 {
+	[SerializeField] private int _damageDealt = 5;
+
 	public override void Execute(BattleBoard board)
 	{
-		throw new System.NotImplementedException();
+		board.Hero.TakeDamage(_damageDealt);
 	}
 }
